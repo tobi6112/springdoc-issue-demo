4c562269bab5471bcf5385f8ad7ce444a3030e14
tobi6112 <22715034+tobi6112@users.noreply.github.com>
2021-02-08 19:15:10 +0100

    New Issue

--- src/main/java/com/example/demo/application/FooController.java
+++ src/main/java/com/example/demo/application/FooController.java
@@ -1,31 +1,26 @@
 package com.example.demo.application;
 
-import java.util.UUID;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.hateoas.EntityModel;
-import org.springframework.hateoas.MediaTypes;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FooController {
 
   private final FooService fooService;
-  private final FooResourceAssembler fooResourceAssembler;
 
   @Autowired
   public FooController(
-      FooService fooService,
-      FooResourceAssembler fooResourceAssembler) {
+      FooService fooService) {
     this.fooService = fooService;
-    this.fooResourceAssembler = fooResourceAssembler;
   }
 
-  @GetMapping(value = "foo/{id}", produces = MediaTypes.HAL_JSON_VALUE)
-  public ResponseEntity<EntityModel<Foo>> getFoo(@PathVariable("id") UUID id) throws Exception {
-    var foo = fooService.getFoo(id).orElseThrow(Exception::new);
-    return ResponseEntity.ok(fooResourceAssembler.toModel(foo));
+  @GetMapping(value = "foos")
+  public ResponseEntity<List<Foo>> getFoo(Sort sort) throws Exception {
+    var foo = fooService.getFoos();
+    return ResponseEntity.ok(foo);
   }
 }
--- src/main/java/com/example/demo/application/FooService.java
+++ src/main/java/com/example/demo/application/FooService.java
@@ -1,12 +1,15 @@
 package com.example.demo.application;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 import org.springframework.stereotype.Service;
 
 @Service
 public class FooService {
-  public Optional<Foo> getFoo(UUID uuid) {
-    return Optional.of(new Foo(uuid.toString()));
+  public List<Foo> getFoos() {
+    return Arrays.asList(new Foo(UUID.randomUUID().toString()), new Foo(UUID.randomUUID().toString()));
   }
 }
